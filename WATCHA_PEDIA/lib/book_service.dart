95403c2597e002b74519a2f2b3ca51a7d836bded
KimSeoWon <150943603+ksw1912@users.noreply.github.com>
2024-06-26 05:39:27 +0900

    Add files via upload

---
+++ WATCHA_PEDIA/lib/book_service.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+import 'Book.dart';
+
+class BookService extends ChangeNotifier {
+  List<Book> bookList = []; // 책 목록
+  List<Book> likedBookList = [];
+
+  void search(String q) async {
+    bookList.clear();
+    if (q.isNotEmpty) {
+      Response res = await Dio().get(
+        "https://www.googleapis.com/books/v1/volumes?q=$q&startIndex=0&maxResults=40",
+      );
+      List items = res.data["items"];
+
+      for (Map<String, dynamic> item in items) {
+        Book book = Book(
+          id: item['id'],
+          title: item['volumeInfo']['title'] ?? "",
+          subtitle: item['volumeInfo']['subtitle'] ?? "",
+          thumbnail: item['volumeInfo']['imageLinks']?['thumbnail'] ??
+              "https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg",
+          previewLink: item['volumeInfo']['previewLink'] ?? "",
+        );
+        bookList.add(book);
+      }
+    }
+    notifyListeners();
+  }
+
+  void toggleLikeBook({required Book book}) {
+    String bookId = book.id;
+    if (likedBookList.map((book) => book.id).contains(bookId)) {
+      likedBookList.removeWhere((book) => book.id == bookId);
+    } else {
+      likedBookList.add(book);
+    }
+    notifyListeners();
+  }
+}
